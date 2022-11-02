4ccd8e22ae309817a45d663d043917bfdfdaa2d8
Susheel Kumar <sushilkumar8081742575@gmail.com>
2022-11-02 11:48:03 +0530

    code fixed susheel

--- Prime.js
+++ Prime.js
@@ -1,9 +1,16 @@
 function isPrime(number){
 	count=0;
-	for(let i=2; i<=number; i++){
+	for(let i=1; i<=number; i++){
 		if(number%i==0){
-			return true; 
+			count++ 
 		}
 	}
+     return count;
 }
-let ans isPrime(7);
+
+let ans = isPrime(7);
+if(ans==2){
+	console.log("Prime Number")
+}else{
+	console.log("Not a Prime Number")
+}
